987aaf58d0e50fa24df499a8acfc6f611c2a36cc
ShreyaSomvanshi <shreyamisthi@gmail.com>
2026-07-12 12:33:31 +0530

    frontend resource allocation

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+// src/App.jsx
+import React from 'react';
+import ResourceBooking from './ResourceBooking.jsx';
+
+function App() {
+  return (
+    <div>
+      <ResourceBooking />
+    </div>
+  );
+}
+
+export default App;
