3e8d6d0e7b2c0c42b3cd8afcc16cf911606e4c6c
ShreyaSomvanshi <shreyamisthi@gmail.com>
2026-07-12 11:02:10 +0530

    commit dashboard

--- Backend/index.js
+++ Backend/index.js
@@ -1,8 +1,8 @@
-// index.js
 import dotenv from 'dotenv';
-import app from './app.js';
 
 dotenv.config();
+
+const app = (await import('./app.js')).default;
 
 const PORT = process.env.PORT || 5000;
 
